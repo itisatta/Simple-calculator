3805696e4d51e14d95507708c279680d7af774c2
Atta Mohajjel <86556686+itisatta@users.noreply.github.com>
2021-12-13 11:58:36 +0330

    Add files via upload

---
+++ simplecalculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Dear user \n";
+    cout<<"Pleas fill the quistions\n";
+   
+    int num1 , num2 ;
+   
+    cout<<"First number: ";
+    cin >> num1 ;
+
+    cout<<"Second number: ";
+    cin >> num2 ;
+
+    cout<< "Plus is equal to " << num1 + num2;
+    cout<< "Minus is equal to " << num1 - num2 ;
+    
+    return 0;
+}
